0ebf83d8c7d3373eeb883877030a941a46f299c2
anvlaibar <olav@haug1.no>
2018-11-17 16:44:16 +0100

    Update Dto.js

--- src/pages/home/Dto.js
+++ src/pages/home/Dto.js
@@ -2,14 +2,16 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var Dto = /** @class */ (function () {
-        function Dto(name, image, price, location, zip, rating, hours) {
-            this.name = name;
-            this.image = image;
-            this.price = price;
-            this.location = location;
-            this.zip = zip;
-            this.rating = rating;
-            this.hours = hours;
+        class Dto {
+            constructor(name, image, price, location, zip, rating, hours) {
+                this.name = name;
+                this.image = image;
+                this.price = price;
+                this.location = location;
+                this.zip = zip;
+                this.rating = rating;
+                this.hours = hours;
+            }
         }
         return Dto;
     }());
