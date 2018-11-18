1362dc5b35210a03eff7b561083b15b59d7c8d61
anvlaibar <olav@haug1.no>
2018-11-18 12:36:17 +0100

    Added review section to DetailPage

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -2,6 +2,7 @@ import { ToastController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import { Dto } from '../home/Dto';
+
 /**
  * Generated class for the DetailPage page.
  *
@@ -21,6 +22,14 @@ export class DetailPage {
   reviews: any[];
   bookmarked: boolean = false;
   bookmarkText: string = "Bookmark";
+  
+  
+  comments:any[];
+  authour:string;
+  timestamp:string;
+  message:string;
+  profileImg:string;
+  rating:number;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams, public events: Events,
@@ -32,7 +41,13 @@ export class DetailPage {
       "../../assets/imgs/thumbnail/Restaurant/gallery/crop2.jpg",
       "../../assets/imgs/thumbnail/Restaurant/gallery/crop3.jpg",
       "../../assets/imgs/thumbnail/Restaurant/gallery/crop4.jpg"
-    ]
+    ];
+
+    this.comments = [
+      new Comment('Ivonne Steil','1. November 2018', 'We went over to ' + this.product.name + ' after landing in Oslo, and we were dying for a good meal! Our hotel was just down the street from ' + this.product.name + ' and so we decided to go give it a shot! (...)','assets/imgs/detail/comments/image-23.png',5),
+      new Comment('Bernard Perkin','14. March 2015', 'For the uninitiated, there is the ' + this.product.name + '. I highly recommend going to the (...) ','assets/imgs/detail/comments/image-7.png',4),
+      new Comment('Sharen Marquart','23. August 2017', 'The award of Oslo\'s worst Cappuccino goes to ' + this.product.name + '. It takes a special kind of skill to mess up coffee this much.','assets/imgs/detail/comments/image-44.png',1)
+    ];
   }
 
   bookmark() {
@@ -50,4 +65,18 @@ export class DetailPage {
     } 
   toast.present();
   }
+}
+
+
+function Comment(
+  authour:string,
+  timestamp:string,
+  message:string,
+  profileImg:string,
+  rating:number) {
+  this.author = authour;
+  this.timestamp = timestamp;
+  this.message = message;
+  this.profileImg = profileImg;
+  this.rating = rating;
 }
